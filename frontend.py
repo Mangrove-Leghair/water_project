78ff65e6f07dd9687fd612e44793ad7834bafda2
vedant <vedantm10@gmail.com>
2023-02-07 16:07:54 +0530

    Version 1.7 Working Properly Simple

--- frontend.py
+++ frontend.py
@@ -5,11 +5,12 @@ import plotly.express as px
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
-
+#API credentials
 API_URL = 'https://tsaepqgxuguuoobkpivv.supabase.co'
 API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzYWVwcWd4dWd1dW9vYmtwaXZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3NTgyNDMsImV4cCI6MTk5MTMzNDI0M30.vvY4PjW68WDYgKjrS3c2IXqP0IiXV1cAj7eGpgNkDLw'
 supabase = create_client(API_URL, API_KEY)
 
+#Fetching data from supabase database
 supabaseList = supabase.table('Water Level').select('*').execute().data
 
 # time range variables updation to put in x axis range parameters
@@ -20,7 +21,9 @@ present_date = today.strftime("%Y-%m-%d %X")
 twodaysago = today - \
         timedelta(days = 2)
 back_date = twodaysago.strftime("%Y-%m-%d %X")
+custom_range = [back_date, present_date] # Setting time values for y axis to show the time period
 
+#initialize dataframe
 df = pd.DataFrame()
 
 for row in supabaseList:
@@ -30,12 +33,12 @@ for row in supabaseList:
     row["DateTime"] = row["created_at"]
     df = df.append(row, ignore_index=True)
 
+#creating local list
+
+#Display section
 orignal_title = '<h1 style="font-family:Helvetica; color:Black; font-size: 45px; text-align: center">Tridev Water Monitoring System</p>'
 st.markdown(orignal_title, unsafe_allow_html=True)
 st.text("")
-
-custom_range = [back_date, present_date]
-
 fig = px.area(df, x="DateTime", y="water_level", title='',markers=False)
 fig.update_layout(
     title={
@@ -55,5 +58,6 @@ fig.add_hline(y=80, line_width=3, line_color="black",
               annotation_font_color="black"
               )
 
+#Final Chart print
 st.plotly_chart(fig,use_container_width=True)
 
